1ebe3fcb2159b15cc4e058d8935fa9efbca1e56f
Nigel Jacob <66517600+The-H4CKER@users.noreply.github.com>
2021-09-11 00:14:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,50 +7,56 @@ import RPi.GPIO as GPIO
 from threading import Thread
 
 
-print("Press 'q' to exit.")
+def analyse() -> None:
+    print("Press 'q' to exit.")
 
-while not cv.waitKey(1) & 0xFF == ord("q"):
-    try:
-        (success, img) = config.cap.read()
-        (classIds, confs, bbox) = config.net.detect(img, confThreshold=config.ACCURACY)
+    while not cv.waitKey(1) & 0xFF == ord("q"):
+        try:
+            (success, img) = config.cap.read()
+            (class_ids, confs, bbox) = config.net.detect(img, confThreshold=config.ACCURACY)
 
-    except cv.error as e:
-        break
+        except cv.error as e:
+            print(e)
+            break
 
-    if len(classIds) != 0:
-        for (classId, confidence, box) in zip(
-            classIds.flatten(), confs.flatten(), bbox
-        ):
-            objectIdentified = config.classNames[classId - 1].upper()
-            probCorrect = round(confidence * 100, 2)
+        if len(class_ids) != 0:
+            for (classId, confidence, box) in zip(
+                    class_ids.flatten(), confs.flatten(), bbox
+            ):
+                object_identified = config.classNames[classId - 1].upper()
+                prob_correct = round(confidence * 100, 2)
 
-            if objectIdentified == "PERSON":
-                color = config.RED
-                # Start alarm if there isn't noise already
-                if config.firstRun:
-                    alarm = Thread(target=config.alert)
-                    config.firstRun = False
-                if not alarm.is_alive():
-                    alarm = Thread(target=config.alert)
-                    alarm.start()
-            else:
-                color = config.GREEN
-            # Draw box around image and label object identified
-            cv.rectangle(img, box, color=color, thickness=2)
-            cv.putText(
-                img,
-                "%s %d%%" % (objectIdentified, probCorrect),
-                (box[0] + 5, box[1] + 20),
-                cv.FONT_HERSHEY_COMPLEX_SMALL,
-                1,
-                color,
-                2,
-            )
+                if object_identified == "PERSON":
+                    color = config.RED
+                    # Start alarm if there isn't noise already
+                    if config.firstRun:
+                        alarm = Thread(target=config.alert)
+                        config.firstRun = False
+                    if not alarm.is_alive():
+                        alarm = Thread(target=config.alert)
+                        alarm.start()
+                else:
+                    color = config.GREEN
+                # Draw box around image and label object identified
+                cv.rectangle(img, box, color=color, thickness=2)
+                cv.putText(
+                    img,
+                    "%s %d%%" % (object_identified, prob_correct),
+                    (box[0] + 5, box[1] + 20),
+                    cv.FONT_HERSHEY_COMPLEX_SMALL,
+                    1,
+                    color,
+                    2,
+                )
 
-    cv.imshow("Security Camera Feed", img)
+        cv.imshow("Security Camera Feed", img)
 
-    if config.button.is_pressed:
-        break
+        if config.button.is_pressed:
+            break
 
-cv.destroyAllWindows()
-GPIO.cleanup()
+    cv.destroyAllWindows()
+    GPIO.cleanup()
+
+
+if __name__ == "__main__":
+    analyse()
